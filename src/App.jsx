b07ddc38476ddf9f4be606aae410a140c80a400c
Ian Burrett <ian@weestoater.com>
2023-10-09 21:18:40 +0100

    testing video files

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import movie from './assets/video.mp4';
 import './App.scss'
 
 function App() {
@@ -25,6 +26,9 @@ function App() {
           Edit <a href="http://stackoverflow.com">src/App.jsx</a> and save to test HMR
         </p>
       </div>
+      <video width="480" height="640" controls>
+        <source src={movie} type="video/mp4" />
+      </video>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
